9eb6f725f33c382ba0f764295445817b2a4ea7bb
Gary Gregory Rogers <gagregrog@gmail.com>
2023-07-29 12:57:09 -0700

    chore: typo

--- include/beeb-boop-beep.h
+++ include/beeb-boop-beep.h
@@ -2,7 +2,7 @@
 
 #include <Arduino.h>
 
-uint16_t bbboteFrequencies[] = {349, 523, 440};
+uint16_t noteFrequencies[] = {349, 523, 440};
 uint16_t noteDurations[] = {62, 62, 62};
 uint16_t notePauses[] = {62, 62, 187};
 uint16_t numberOfNotes = 3;
