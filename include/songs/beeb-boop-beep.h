904c3f8850436d8e75c1b363e5f71f763f08f72b
Gary Gregory Rogers <gagregrog@gmail.com>
2023-07-29 10:13:01 -0700

    chore: move songs

---
+++ include/songs/beeb-boop-beep.h
@@ -0,0 +1,8 @@
+// https://dopeloop.ai/melody-generator
+
+#ifdef BEEP_BOOP_BEEP
+int bbbFrequencies[] = {349,523,440};
+int bbbDurations[] = {62,62,62};
+int bbbPauses[] = {62,62,187};
+int bbbNumberOfNotes = 3;
+#endif
--- songs/beeb-boop-beep.h
+++
@@ -1,6 +0,0 @@
-// https://dopeloop.ai/melody-generator
-
-int noteFrequencies[] = {349,523,440};
-int noteDurations[] = {62,62,62};
-int notePauses[] = {62,62,187};
-int numberOfNotes = 3;
